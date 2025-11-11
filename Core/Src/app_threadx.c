7dfc1884061df22a384ee4f04cec03ce4a18df78
psmey <psmey-dev@protonmail.com>
2025-11-11 17:23:13 +0100

    implement base for tasks

--- Core/Src/app_threadx.c
+++ Core/Src/app_threadx.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "main.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -33,7 +33,8 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define TRACEX_BUFFER_SIZE 64000
+#define THREAD_STACK_SIZE 1024
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -43,13 +44,22 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
-#define TRACEX_BUFFER_SIZE 64000
-uint8_t tracex_buffer[64000];
+uint8_t tracex_buffer[TRACEX_BUFFER_SIZE];
+
+uint8_t speedy_thread_stack[THREAD_STACK_SIZE];
+TX_THREAD speedy_thread_ptr;
+
+uint8_t slow_thread_stack[THREAD_STACK_SIZE];
+TX_THREAD slow_thread_ptr;
+
+TX_MUTEX mutex_ptr;
+UINT status;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN PFP */
-
+void speedy_thread_entry(ULONG initial_input);
+void slow_thread_entry(ULONG initial_input);
 /* USER CODE END PFP */
 
 /**
@@ -65,7 +75,35 @@ UINT App_ThreadX_Init(VOID *memory_ptr)
   /* USER CODE BEGIN App_ThreadX_Init */
   (void)byte_pool;
 
-  tx_trace_enable(&tracex_buffer,TRACEX_BUFFER_SIZE,30);
+  tx_trace_enable(&tracex_buffer, TRACEX_BUFFER_SIZE, 30);
+
+  tx_thread_create(
+	&speedy_thread_ptr,
+	"speedy_thread",
+	speedy_thread_entry,
+	0x01234,
+	speedy_thread_stack,
+	THREAD_STACK_SIZE,
+	5,
+	5,
+	TX_NO_TIME_SLICE,
+	TX_AUTO_START
+  );
+
+  tx_thread_create(
+	&slow_thread_ptr,
+	"slow_thread",
+	slow_thread_entry,
+	0x01234,
+	slow_thread_stack,
+	THREAD_STACK_SIZE,
+	15,
+	15,
+	TX_NO_TIME_SLICE,
+	TX_AUTO_START
+  );
+
+  status = tx_mutex_create(&mutex_ptr, "mutex_ptr", TX_NO_INHERIT);
   /* USER CODE END App_ThreadX_Init */
 
   return ret;
@@ -90,5 +128,39 @@ void MX_ThreadX_Init(void)
 }
 
 /* USER CODE BEGIN 1 */
+void speedy_thread_entry(ULONG initial_input)
+{
+	while(1)
+	{
+		tx_thread_sleep(2);
 
+		status = tx_mutex_get(&mutex_ptr, TX_WAIT_FOREVER);
+		HAL_Delay(5);
+		status = tx_mutex_put(&mutex_ptr);
+
+		tx_thread_sleep(4);
+
+		status = tx_mutex_get(&mutex_ptr, TX_WAIT_FOREVER);
+		HAL_Delay(3);
+		status = tx_mutex_put(&mutex_ptr);
+	}
+}
+
+void slow_thread_entry(ULONG initial_input)
+{
+	while(1)
+	{
+		status = tx_mutex_get(&mutex_ptr, TX_WAIT_FOREVER);
+		HAL_Delay(12);
+		status = tx_mutex_put(&mutex_ptr);
+
+		tx_thread_sleep(8);
+
+		status = tx_mutex_get(&mutex_ptr, TX_WAIT_FOREVER);
+		HAL_Delay(11);
+		status = tx_mutex_put(&mutex_ptr);
+
+		tx_thread_sleep(9);
+	}
+}
 /* USER CODE END 1 */
--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # HAW Kiel Embedded Systems
 
-## Notes
+## Tasks
 
-### Add Azure RTOS ThreadX Support
+### 4.6 TraceX
+
+Add Azure RTOS ThreadX Support
 
 https://community.st.com/t5/stm32-mcus/how-can-i-add-tracex-support-in-stm32cubeide/ta-p/49380
+
+
+![Result 4.6](assets/result_4_6.png)
+
+### 4.7 Critical sections
+
+Slow thread gets interrupted!
+
+![Result 4.7 without prio inheritance](assets/result_4_7_without_prio_inheritance.png)
+
+Slow thread gains in priority until it also has a priority of 5.
+
+![Result 4.7 with prio inheritance](assets/result_4_7_with_prio_inheritance.png)
